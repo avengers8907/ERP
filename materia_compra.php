f43f437974851ad1448780e55399ca74c9797aa9
Raul <raul89071626@gmail.com>
2019-01-21 19:05:18 -0600

    decima version

--- materia_compra.php
+++ materia_compra.php
@@ -8,22 +8,30 @@ public function alta(){
 $materia=$_POST["materia"];
 $compra=$_POST["compra"];
 $cantidad=$_POST["cantidad"];
-$this->sentencia = "Insert into materia_compra values(null,'$materia','$compra','$cantidad');";
+$this->sentencia = "INSERT INTO materia_compra values(null,'$materia','$compra','$cantidad');";
 $this->ejecutar_sentencia();
 
 }
 
 public function baja(){
-	
+	$idEliminar = $_POST["idEliminar"];
+	$this->sentencia= "DELETE FROM materia_compra WHERE id_materiacompra=$idEliminar";
+	$this->ejecutar_sentencia();
 
 }
 
 public function consulta(){
-
+$this->sentencia = "SELECT * FROM materia_compra";
+return $this->obtener_sentencia();
 }
 
 public function modifica(){
-
+$idModificar = $_POST["idModificar"];
+$materia=$_POST["materia"];
+$compra=$_POST["compra"];
+$cantidad=$_POST["cantidad"];
+$this->sentencia = "UPDATE materia_compra SET materia='$materia',compra='$compra',cantidad='$cantidad' where id_materiacompra=$idModificar";
+$this->ejecutar_sentencia();
 }
 
 }
