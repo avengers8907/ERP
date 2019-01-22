17122f7dd7f608ef2cb3a2df8f95994705586852
Raul <raul89071626@gmail.com>
2019-01-21 18:59:53 -0600

    quinta version

--- cliente.php
+++ cliente.php
@@ -10,21 +10,32 @@ $razon=$_POST["razon"];
 $correo=$_POST["correo"];
 $direccion=$_POST["direccion"];
 $telefono=$_POST["telefono"];
-$this->sentencia = "Insert into cliente values(null,'$nombre','$razon','$correo','$direccion','$telefono');";
+$this->sentencia = "INSERT INTO cliente values(null,'$nombre','$razon','$correo','$direccion','$telefono');";
 $this->ejecutar_sentencia();
 
 }
 
 public function baja(){
-	
+	$idEliminar = $_POST["idEliminar"];
+	$this->sentencia= "DELETE FROM cliente WHERE id_cliente=$idEliminar";
+	$this->ejecutar_sentencia();
 
 }
 
 public function consulta(){
-
+$this->sentencia = "SELECT * FROM cliente";
+return $this->obtener_sentencia();
 }
 
 public function modifica(){
+idModificar =$_POST["idModificar"];
+$nombre=$_POST["nombre"];
+$razon=$_POST["razon"];
+$correo=$_POST["correo"];
+$direccion=$_POST["direccion"];
+$telefono=$_POST["telefono"];
+$this->sentencia = "UPDATE cliente SET nombre='$nombre',razon='$razon',correo='$correo',direccion='$direccion',telefono='$telefono' where id_cliente=$idModificar";
+$this->ejecutar_sentencia();
 
 }
 
