7ca6422fd7174eabaf58ded78c7dc874c657876d
Raul <raul89071626@gmail.com>
2019-01-21 19:02:07 -0600

    segunda version

--- finanzas.php
+++ finanzas.php
@@ -8,21 +8,31 @@ public function alta(){
 $inicio=$_POST["inicio"];
 $fin=$_POST["fin"];
 $total=$_POST["total"];
-$this->sentencia = "Insert into finanzas values(null,'$inicio','$fin','$total');";
+$this->sentencia = "INSERT INTO finanzas values(null,'$inicio','$fin','$total');";
 $this->ejecutar_sentencia();
 
 }
 
 public function baja(){
-	
+	$idEliminar = $_POST["idEliminar"];
+	$this->sentencia= "DELETE FROM finanzas WHERE id_finanzas=$idEliminar";
+	$this->ejecutar_sentencia();
 
 }
 
 public function consulta(){
-
+$this->sentencia = "SELECT * FROM finanzas";
+return $this->obtener_sentencia();
 }
 
 public function modifica(){
+	$idModificar = $_POST["idModificar"];
+	$inicio=$_POST["inicio"];
+$fin=$_POST["fin"];
+$total=$_POST["total"];
+$this->sentencia = "UPDATE finanzas SET inicio='$inicio',fin='$fin',total='$total'  where id_finanzas=$idModificar";
+$this->ejecutar_sentencia();
+
 
 }
 
