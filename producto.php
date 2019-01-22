1857560631bd073827fe658e8b59f912096595ba
Raul <raul89071626@gmail.com>
2019-01-21 19:06:51 -0600

    onceava version

--- producto.php
+++ producto.php
@@ -14,22 +14,37 @@ $codigo=$_POST["codigo"];
 $descripcion=$_POST["descripcion"];
 $almacen=$_POST["almacen"];
 $categoria=$_POST["categoria"];
-$this->sentencia = "Insert into producto values(null,'$nombre','$costo','$unidad','$stock','$existencia','$codigo','$descripcion','$almacen','$categoria');";
+$this->sentencia = "INSERT INTO producto values(null,'$nombre','$costo','$unidad','$stock','$existencia','$codigo','$descripcion','$almacen','$categoria');";
 $this->ejecutar_sentencia();
 
 }
 
 public function baja(){
-	
+	$idEliminar = $_POST["idEliminar"];
+	$this->sentencia= "DELETE FROM producto WHERE id_producto=$idEliminar";
+	$this->ejecutar_sentencia();
 
 }
 
 public function consulta(){
+	$this->sentencia = "SELECT * FROM producto";
+return $this->obtener_sentencia();
 
 }
 
 public function modifica(){
-
+$idModificar = $_POST["idModificar"];
+$nombre=$_POST["nombre"];
+$costo=$_POST["costo"];
+$unidad=$_POST["unidad"];
+$stock=$_POST["stock"];
+$existencia=$_POST["existencia"];
+$codigo=$_POST["codigo"];
+$descripcion=$_POST["descripcion"];
+$almacen=$_POST["almacen"];
+$categoria=$_POST["categoria"];
+$this->sentencia = "UPDATE producto SET nombre='$nombre','$costo','$unidad','$stock','$existencia','$codigo','$descripcion','$almacen','$categoria');";
+$this->ejecutar_sentencia();
 }
 
 }
