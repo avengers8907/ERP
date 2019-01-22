565539307d09741fa23223d9253dd2d37c2db6c9
Raul <raul89071626@gmail.com>
2019-01-21 19:04:50 -0600

    novena version

--- materia.php
+++ materia.php
@@ -10,22 +10,33 @@ $unidad=$_POST["unidad"];
 $existencia=$_POST["existencia"];
 $costo=$_POST["costo"];
 $proveedor=$_POST["proveedor"];
-$this->sentencia = "Insert into materia values(null,'$nombre','$unidad','$existencia','$costo','$proveedor');";
+$this->sentencia = "INSERT INTO materia values(null,'$nombre','$unidad','$existencia','$costo','$proveedor');";
 $this->ejecutar_sentencia();
 
 }
 
 public function baja(){
-	
+	$idEliminar = $_POST["idEliminar"];
+	$this->sentencia= "DELETE FROM materia WHERE id_materia=$idEliminar";
+	$this->ejecutar_sentencia();
 
 }
 
 public function consulta(){
+$this->sentencia = "SELECT * FROM materia";
+return $this->obtener_sentencia();
 
 }
 
 public function modifica(){
-
+	$idModificar = $_POST["idModificar"];
+$nombre=$_POST["nombre"];
+$unidad=$_POST["unidad"];
+$existencia=$_POST["existencia"];
+$costo=$_POST["costo"];
+$proveedor=$_POST["proveedor"];
+$this->sentencia = "UPDATE materia SET nombre='$nombre',unidad='$unidad',existencia='$existencia',costo='$costo',proveedor='$proveedor' WHERE id_materia=$idModificar";
+$this->ejecutar_sentencia();
 }
 
 }
